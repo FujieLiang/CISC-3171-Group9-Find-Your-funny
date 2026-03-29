163ab0e4a177273d416222f4765d5af494f28eb1
Keyshawn Seymour <148930302+BigPoppaK@users.noreply.github.com>
2026-03-29 17:13:38 -0400

    Implement create account endpoint
    Added a new endpoint for creating user accounts with validation and error handling.

--- Backend/main.py
+++ Backend/main.py
@@ -1,12 +1,46 @@
 from flask import request, jsonify
 from config import app, database
-from models import Venue
+from models import *
 
 @app.route("/venues", methods=["GET"])
 def get_venues():
     venues = Venue.query.all()
     jsonVenues = list(map(lambda x: x.to_json(), venues))
     return jsonify({"Venues":jsonVenues})
+
+@app.route("/create-account", methods=["POST"])
+def create_account():
+    id = request.json.get("id")
+    firstName = request.json.get("firstName")
+    lastName = request.json.get("lastName")
+    email = request.json.get("email")
+    password = request.json.get("password")
+    streetAddress = request.json.get("streetAddress")
+    city = request.json.get("city")
+    state = request.json.get("state")
+    country = request.json.get("country")
+    zipCode = request.json.get("zipCode")
+
+    passwordHash = hash(password)
+
+    if not id or not firstName or not lastName or not email or not passwordHash or not streetAddress or not city or not state or not country or not zipCode:
+        return(jsonify({"message":"Incorrect value in submission. Please resubmit."}), 400)
+    
+    newUser = User(id=id, firstName=firstName, lastName=lastName, email=email, passwordHash=passwordHash,streetAddress=streetAddress,city=city,state=state,country=country,zipCode=zipCode)
+    try:
+        database.session.add(newUser)
+        database.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "New user created"}), 201
+    
+@app.route("/events", methods=["GET"])
+def get_venues():
+    events = Events.query.all()
+    jsonVenues = list(map(lambda x: x.to_json(), events))
+    return jsonify({"Events":jsonVenues})
+    
 
 if __name__ == "__main__":
     with app.app_context():
